9f1f101da08237be8797a70468644f7517b914a7
PandaTeja <teja.panda@gmail.com>
2022-03-24 05:12:51 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+
+const App = () => (
+  <div className="container ">
+    <div className="box-box1"></div>
+    <div className="container1">
+      <div className="container2">
+        <div className="box-box2 "></div>
+        <div className="box-box3"></div>
+      </div>
+      <div className="box-box4"></div>
+    </div>
+  </div>
+)
+
+export default App
